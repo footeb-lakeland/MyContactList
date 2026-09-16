d58992363ba39d9fb7d8409ecd8ee5ed3b584aae
Foote, Brian <footeb@lakeland.edu>
2026-09-16 18:56:15 -0500

    Screen cleanup and added the calendar

--- app/src/main/java/edu/bdf/mycontactlist/ContactEditActivity.java
+++ app/src/main/java/edu/bdf/mycontactlist/ContactEditActivity.java
@@ -1,6 +1,9 @@
 package edu.bdf.mycontactlist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,9 +24,42 @@ public class ContactEditActivity extends AppCompatActivity {
             return insets;
         });
 
+        initToggleButton();
         Navbar.initListButton(this);
         Navbar.initMapButton(this);
         Navbar.initSettingsButton(this);
+        setForEditing(false);
+    }
+    private void initToggleButton(){
+        final ToggleButton editToggle = (ToggleButton)findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
     }
 
+    private void setForEditing(boolean enabled) {
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipCode = findViewById(R.id.editZipcode);
+        EditText editHome = findViewById(R.id.editHome);
+        EditText editCell = findViewById(R.id.editCell);
+        EditText editEmail = findViewById(R.id.editEMail);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipCode.setEnabled(enabled);
+        editHome.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+
+        if(enabled)
+            editName.requestFocus();
+    }
 }
