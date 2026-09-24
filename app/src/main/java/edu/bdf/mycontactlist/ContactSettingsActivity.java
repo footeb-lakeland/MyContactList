c7233d94043abf8e7a345723e39ca229810cfd1c
Foote, Brian <footeb@lakeland.edu>
2026-09-23 19:18:41 -0500

    Added the settings java 2

--- app/src/main/java/edu/bdf/mycontactlist/ContactSettingsActivity.java
+++ app/src/main/java/edu/bdf/mycontactlist/ContactSettingsActivity.java
@@ -1,6 +1,9 @@
 package edu.bdf.mycontactlist;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,5 +27,89 @@ public class ContactSettingsActivity extends AppCompatActivity {
         Navbar.initMapButton(this);
         Navbar.initSettingsButton(this);
         this.setTitle("Settings");
+        initSettings();
+        initSortByClick();
+        initSortOrderClick();
+    }
+    private void initSettings() {
+        // Read the sortfied out of the SPs
+        String sortBy = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortfield","contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortorder","ASC");
+
+        RadioButton rbName = findViewById(R.id.radioName);
+        RadioButton rbCity = findViewById(R.id.radioCity);
+        RadioButton rbBirthDay = findViewById(R.id.radioBirthday);
+
+        if (sortBy.equalsIgnoreCase("contactname")) {
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("city")) {
+            rbCity.setChecked(true);
+        }
+        else {
+            rbBirthDay.setChecked(true);
+        }
+
+        RadioButton rbAscending = findViewById(R.id.radioAscending);
+        RadioButton rbDescending = findViewById(R.id.radioDescending);
+        if (sortOrder.equalsIgnoreCase("ASC")) {
+            rbAscending.setChecked(true);
+        }
+        else {
+            rbDescending.setChecked(true);
+        }
+
+
+    }
+
+    private void initSortOrderClick() {
+        RadioGroup rgSortOrder = findViewById(R.id.radioGroupSortOrder);
+        rgSortOrder.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                RadioButton rbAscending = findViewById(R.id.radioAscending);
+                if (rbAscending.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit().putString("sortorder", "ASC").apply();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).edit().putString("sortorder", "DESC").apply();
+                }
+            }
+        });
+    }
+
+    private void initSortByClick() {
+        RadioGroup rgSortBy = findViewById(R.id.radioGroupSortBy);
+        rgSortBy.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                RadioButton rbName = findViewById(R.id.radioName);
+                RadioButton rbCity = findViewById(R.id.radioCity);
+                if (rbName.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE)
+                            .edit()
+                            .putString("sortfield", "contactname")
+                            .apply();
+                }
+                else if (rbCity.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE)
+                            .edit()
+                            .putString("sortfield", "city").apply();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE)
+                            .edit()
+                            .putString("sortfield", "birthday").apply();
+                }
+            }
+        });
     }
 }
